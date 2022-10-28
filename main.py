2f471ff8928dc842d6c54fe9bd5eb9acec91a964
sunny <sunnyroy8084401051@gmail.com>
2022-10-28 22:44:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+"""
+This is many first python code that I'm writing , in code is based on explanation of adding number and
+how to know what kind of data type the variable is storing in it.
+"""
+a=55
+b=65
+c=99
+print(a+b,c)
+print(type(a) ,c)
